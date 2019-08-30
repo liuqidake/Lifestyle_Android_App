5a4d3846b5270b0d1dedf9ec7c5a5fa23e6e8266
morrinic <nicole.morris@utah.edu>
2019-08-30 13:22:26 -0600

    Updated fragment_name_age xml to show basic components

--- Lifestyle_App/app/src/main/java/com/example/nicolemorris/lifestyle/MainActivity.java
+++ Lifestyle_App/app/src/main/java/com/example/nicolemorris/lifestyle/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    String username = "Nicole";
+    String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
