38f7461e5e3ab169ec23c117b40a35d1f98843c0
morrinic <nicole.morris@utah.edu>
2019-08-31 18:54:20 -0600

    Updated main activity to support more user choices, added bottom_buttons and bmi fragments

--- Lifestyle_App/app/src/main/java/com/example/nicolemorris/lifestyle/MainActivity.java
+++ Lifestyle_App/app/src/main/java/com/example/nicolemorris/lifestyle/MainActivity.java
@@ -7,21 +7,53 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    String username;
+    String username = "Nicole";
+    int user_choice = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Find each frame layout, replace with corresponding fragment
+        FragmentTransaction fTrans = getSupportFragmentManager().beginTransaction();
+
         if (username == null) {
             Intent messageIntent = new Intent(this, NewUserActivity.class);
             this.startActivity(messageIntent);
+
+        } else if (user_choice == 0){
+
+            //Launch profile information
+
+        } else if (user_choice == 1){
+
+            //Launch fitness goals
+
+        } else if (user_choice == 2){
+
+            //Launch bmi
+            fTrans.replace(R.id.fl_frag_ph_1,new BmiFragment(),"Choices");
+            fTrans.replace(R.id.fl_frag_ph_2,new BottomButtons(),"Choices");
+
+        } else if (user_choice == 3){
+
+            //Launch find-a-hike
+
+        } else if (user_choice == 4){
+
+            //Launch weather information
+
+        } else if (user_choice == 5){
+
+            //Launch help
+
+        } else {
+
+            //Launch main activity
         }
 
-        //Find each frame layout, replace with corresponding fragment
-        FragmentTransaction fTrans = getSupportFragmentManager().beginTransaction();
-        fTrans.replace(R.id.fl_frag_ph_1,new ChoicesListFragment(),"Choices");
+//        fTrans.replace(R.id.fl_frag_ph_1,new ChoicesListFragment(),"Choices");
         fTrans.commit();
 
     }
