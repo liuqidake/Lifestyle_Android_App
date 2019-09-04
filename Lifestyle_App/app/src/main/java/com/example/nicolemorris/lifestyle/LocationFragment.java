0ae2f7572cb29bdac7d565a87bab447111cf2fe6
liuqidake <qiliuchina@yahoo.com>
2019-09-04 11:46:45 -0600

    add location

--- Lifestyle_App/app/src/main/java/com/example/nicolemorris/lifestyle/LocationFragment.java
+++ Lifestyle_App/app/src/main/java/com/example/nicolemorris/lifestyle/LocationFragment.java
@@ -1,17 +1,151 @@
 package com.example.nicolemorris.lifestyle;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import java.util.List;
+import java.util.Locale;
 
-public class LocationFragment extends Fragment {
+public class LocationFragment extends Fragment implements View.OnClickListener{
+
+    private String city, state;
+    private EditText cityText, stateText;
+    private Button locate, next;
+
+    private LocationManager locationManager;
+
+    int REQUEST_LOCATION = 1;
+    double latitude, longitude;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false);
+        View view = inflater.inflate(R.layout.fragment_location, container, false);
+        cityText = view.findViewById(R.id.et_city);
+        stateText = view.findViewById(R.id.et_state);
+        locate = view.findViewById(R.id.b_location);
+        next = view.findViewById(R.id.b_next);
+
+        locate.setOnClickListener(this);
+        next.setOnClickListener(this);
+
+        return view;
     }
+
+    @Override
+    public void onClick(View view){
+        switch(view.getId()){
+            case R.id.b_location:{
+                findCoordinates();
+                System.out.print(latitude+"ooo");
+                Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
+                try{
+                    List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+                    city = addresses.get(0).getLocality();
+                    state = addresses.get(0).getAdminArea();
+                    cityText.setText(city);
+                    stateText.setText(state);
+                } catch(Exception e){
+                    Toast.makeText(getContext(), "Can't located you. Are you at Area 51??", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }
+    }
+
+    private void findCoordinates(){
+        locationManager=(LocationManager)getContext().getSystemService(Context.LOCATION_SERVICE);
+        //Check gps is enable or not
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+        {
+            //Enable gps
+            OnGPS();
+        }
+        else
+        {
+            //Get latitude and longitude and open map based on the location
+            getLocation();
+        }
+    }
+
+    private void getLocation() {
+
+        //Check Permissions again
+
+        if (ActivityCompat.checkSelfPermission(getContext(),Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(),
+
+                Manifest.permission.ACCESS_COARSE_LOCATION) !=PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(getActivity(),new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        }
+        else
+        {
+            Location LocationGps= locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            Location LocationNetwork=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            Location LocationPassive=locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+
+            if (LocationGps !=null)
+            {
+                latitude=LocationGps.getLatitude();
+                longitude=LocationGps.getLongitude();
+            }
+            else if (LocationNetwork !=null)
+            {
+                latitude=LocationNetwork.getLatitude();
+                longitude=LocationNetwork.getLongitude();
+            }
+            else if (LocationPassive !=null)
+            {
+                latitude=LocationPassive.getLatitude();
+                longitude=LocationPassive.getLongitude();
+            }
+            else
+            {
+                Toast.makeText(getContext(), "Can't Get Your Location", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    private void OnGPS() {
+
+        final AlertDialog.Builder builder= new AlertDialog.Builder(getContext());
+
+        builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+        }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                dialog.cancel();
+            }
+        });
+
+        final AlertDialog alertDialog=builder.create();
+        alertDialog.show();
+    }
+
 }
