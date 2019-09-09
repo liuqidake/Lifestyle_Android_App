8f5d8f1aa168ec3aa9445e430638ccc1f5a966e1
sophia1869 <pojundijie@gmail.com>
2019-09-09 12:40:26 -0600

    pass data around last Thurs Hui

--- Lifestyle_App/app/src/main/java/com/example/nicolemorris/lifestyle/ProfilePicFragment.java
+++ Lifestyle_App/app/src/main/java/com/example/nicolemorris/lifestyle/ProfilePicFragment.java
@@ -8,7 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class ProfilePicFragment extends Fragment
+public class
+ProfilePicFragment extends Fragment
         implements View.OnClickListener {
 
     Button bTakePic,bSelectPic,bNext;
