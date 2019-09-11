98f743645cdee167b44c19bb490c4399046cd8f4
liuqidake <qiliuchina@yahoo.com>
2019-09-11 06:31:43 -0600

    store edited profile into database

--- Lifestyle_App/app/src/main/java/com/example/nicolemorris/lifestyle/ChangeProfileFragment.java
+++ Lifestyle_App/app/src/main/java/com/example/nicolemorris/lifestyle/ChangeProfileFragment.java
@@ -1,28 +1,34 @@
 package com.example.nicolemorris.lifestyle;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.example.nicolemorris.lifestyle.Database.DatabaseHelper;
 
-public class ChangeProfileFragment extends Fragment
-        implements AdapterView.OnItemSelectedListener, View.OnClickListener {
+import java.util.Calendar;
+
+public class ChangeProfileFragment extends DialogFragment
+        implements AdapterView.OnItemSelectedListener, DatePickerDialog.OnDateSetListener, View.OnClickListener {
 
     EditText etName, etCity, etState;
     Spinner s_h_feet,s_h_inches,s_w_pounds,s_sex;
-    String dHeight,dWeight,dSex;
+    Calendar date;
     String[] dataToPass;
     Button bSave;
 
-    String name, city, state;
+    String name, city, state, weight, sex;
+    int feet, inches, age;
 
     //Add database
     DatabaseHelper db;
@@ -67,16 +73,16 @@ public class ChangeProfileFragment extends Fragment
                                int pos, long id) {
         switch (view.getId()){
             case R.id.s_feet: {
-                //parent.getItemAtPosition()
+                feet = (Integer)parent.getItemAtPosition(pos);
             }
             case R.id.s_inches: {
-
+                inches = (Integer)parent.getItemAtPosition(pos);
             }
             case R.id.s_weight: {
-
+                weight = (String)parent.getItemAtPosition(pos);
             }
             case R.id.s_sex: {
-
+                sex = (String)parent.getItemAtPosition(pos);
             }
         }
 
@@ -92,9 +98,33 @@ public class ChangeProfileFragment extends Fragment
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.b_save: {
+                name = etName.getText().toString();
+                city = etCity.getText().toString();
+                state = etState.getText().toString();
+                db.updateData(name, age, city, state, feet, inches, weight, sex);
                 break;
             }
         }
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        // Do something with the date chosen by the user
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(view.getYear(), view.getMonth(), view.getDayOfMonth());
+        this.date = calendar;
+        age = Calendar.getInstance().get(Calendar.YEAR) - this.date.get(Calendar.YEAR);
     }
 
 }
